30045ea356f5794cf45060f72c3a4f138c8e4b8c
sebnapi <sebnapi@gmail.com>
2023-09-02 20:09:00 +0100

    Fixed problems with code

--- gym_futures/__init__.py
+++ gym_futures/__init__.py
@@ -1,5 +1,9 @@
 from gym.envs.registration import register
 
+from envs.futures_env import FuturesEnv
+from envs.utils import TimeSeriesState
+
+
 register(
     id='futures-v0',
     entry_point='gym_futures.envs:FuturesEnv',
--- gym_futures/envs/utils.py
+++ gym_futures/envs/utils.py
@@ -4,6 +4,9 @@ import math
 import pandas 
 import numpy
 from operator import itemgetter
+import json
+import numpy as np
+
 
 def round_to_nearest_increment(x :float, tick_size : float = 0.25) -> float:
   """
@@ -21,6 +24,20 @@ def round_to_nearest_increment(x :float, tick_size : float = 0.25) -> float:
     return tick_size
   else:
     return val
+
+
+
+class NumpyEncoder(json.JSONEncoder):
+    """ Special json encoder for numpy types """
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
 
 def monotonicity(series: Union[pandas.Series, pandas.DataFrame, numpy.ndarray]) -> float:
   """
